814ade9755bda1882518dc89d6853eb6ca715dc2
arwa Ahmed <0arwagalal0@gmail.com>
2020-07-24 18:47:54 +0200

    first commit

---
+++ client/build/precache-manifest.c26476705b7473e6c7ea6c907a880e27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767005c2d1ff19485e8cac68f5adef2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42004ede89625f53c8bd",
+    "url": "/static/css/main.5fd38d68.chunk.css"
+  },
+  {
+    "revision": "8c0e94f2953163746286",
+    "url": "/static/js/2.6e982244.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.6e982244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42004ede89625f53c8bd",
+    "url": "/static/js/main.f5d98832.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
